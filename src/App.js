67e4ed9c556fe8373d8e77de0833b0f55feafaae
Pallab Jyoti Singha <pallabjyotisingha@gmail.com>
2021-08-26 17:00:19 +0530

    homepage added (change)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
     console.log(process.env["REACT_APP_SERVER_URL"]);
-    return <div className="App"> App is Online </div>;
+    return <div className="App"> App is Online, Yay! </div>;
 }
 
 export default App;
