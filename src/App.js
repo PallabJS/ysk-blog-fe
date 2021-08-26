14c78528304344cfe77f4521261dc57d72b0729d
Pallab Jyoti Singha <pallabjyotisingha@gmail.com>
2021-08-26 11:29:39 +0530

    Change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
     console.log(process.env["REACT_APP_SERVER_URL"]);
-    return <div className="App">HELLO</div>;
+    return <div className="App"> App is Online </div>;
 }
 
 export default App;
