0e1553608edb9cb7d1ec83e51323488371efc7f0
Pallab Jyoti Singha <pallabjyotisingha@gmail.com>
2021-09-12 04:45:20 +0530

    fixed unsupported hooks import from cjs

--- src/components/Sidecontent/Sidecontent.js
+++ src/components/Sidecontent/Sidecontent.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useEffect, useState } from "react/cjs/react.development";
+import React, { useEffect, useState } from "react";
 
 import { postApi } from "../../api/post/postapi";
 import { PostCard } from "../postcomponents/Post";
