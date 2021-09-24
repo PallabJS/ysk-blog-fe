218265b9e38b7d48393a8e81adc7621a8c8f6356
Pallab Jyoti Singha <pallabjyotisingha@gmail.com>
2021-09-24 23:32:39 +0530

    ci-cd reloaded

--- src/components/offline/Errormsg.js
+++ src/components/offline/Errormsg.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { serverUrl } from "../../settings";
 
 import "./errormsg.scss";
 
 const Errormsg = (props) => {
+    useEffect(async () => {
+        await fetch(serverUrl + "/404", { method: "get" });
+    }, []);
     return (
         <div className="offline">
             <h1>{props.title}</h1>
