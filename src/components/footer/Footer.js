431902fa369d1f4623e458c66fac6b18c105a921
unknown <pallabjyotisingha@gmail.com>
2022-01-04 09:07:17 +0530

    updated the ui theme colors ->  1.0.1 released

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,45 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { userApi } from "../../api/user/userapi";
 
 import "./footer.scss";
 
 const Footer = () => {
-    const [subscriptions, setSubscriptions] = useState({
-        tech: false,
-        science: false,
-        news: false,
-    });
-    const [emailAddress, setEmailAddress] = useState("");
+    const [show, setShow] = useState(false);
 
-    const handleSubsToggle = (e) => {
-        let category = e.target.value;
-        setSubscriptions({ ...subscriptions, [category]: !subscriptions[category] });
-    };
-
-    const addToMailingList = () => {
-        let data = {
-            email: emailAddress,
-            subscriptions: Object.keys(subscriptions).filter((category) => subscriptions[category]),
-        };
-        userApi.addToMailingList(data).then((res) => {
-            if (res.error) {
-                window.alert(res.msg);
-            } else {
-                window.alert("You are subscribed to newsletter, Congrats!");
-            }
-        });
-    };
+    useEffect(() => {
+        setTimeout(() => {
+            setShow(true);
+        }, 5000);
+    }, []);
 
     return (
         <footer className="footer">
-            <div className="footer_copyright">
-                <i>youshouldknow.live</i> &copy; {new Date().getFullYear()}
-            </div>
-
-            {/* <span style={{ color: "lightskyblue", fontWeight: 100, margin: "0px 10px" }}>|</span>
-
-            <a className="contact_us">Contact us</a> */}
+            {show && (
+                <div className="footer_copyright">
+                    Youshouldknow.live &copy; {new Date().getFullYear()}
+                </div>
+            )}
         </footer>
     );
 };
